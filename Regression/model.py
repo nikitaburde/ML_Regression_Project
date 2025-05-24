36693f6e19627bbeaa85d43e13a94e58f2ba2ff3
nikita <nikitaburde01@gmail.com>
2025-05-24 13:53:33 +0200

    MSE and R2 evaluation added

--- Regression/model.py
+++ Regression/model.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 class MyRegression:
     def __init__(self):
@@ -10,5 +11,7 @@ class MyRegression:
     def predict(self, X):
         return self.model.predict(X)
 
-    def evaluate(self, X, y):
-        return self.model.score(X, y)
+    def evaluate(self, y_test, y_pred):
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        return mse, r2
