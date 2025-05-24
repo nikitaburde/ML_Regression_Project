76a25e80c6c1bedfcc6cf0ce766bb62a505d902e
nikita <nikitaburde01@gmail.com>
2025-05-24 12:56:41 +0200

    Regression model created

---
+++ Regression/model.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LinearRegression
+
+class MyRegression:
+    def __init__(self):
+        self.model = LinearRegression()
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def evaluate(self, X, y):
+        return self.model.score(X, y)
